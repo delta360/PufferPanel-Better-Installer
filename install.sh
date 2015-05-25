3ec24d702c8fb58f5200e9ba587a789b4fe2abad
Alex J <alex@delta360.net>
2015-05-25 21:49:19 +0100

    Edited some files

--- install.sh
+++ install.sh
@@ -2,22 +2,44 @@
 echo "PufferPanel - Better Installer";
 if [[ `id -u` -ne 0 ]] ; then echo "This installer can only be run as root." ; exit 1 ; fi
 echo "Checking for dependencies...";
+echo "" >> /etc/apt/sources.list
+echo "deb http://packages.dotdeb.org wheezy-php55 all" >> /etc/apt/sources.list
+echo "deb-src http://packages.dotdeb.org wheezy-php55 all" >> /etc/apt/sources.list
+wget http://www.dotdeb.org/dotdeb.gpg
+apt-key add dotdeb.gpg
+rm -rf dotdeb.gpg
 apt-get update
 echo "Installing dependencies...";
-apt-get install -y openssl curl apache2 git mysql-client mysql-server php5 php5-cli php5-curl php5-mysql php5-mcrypt
-PS3='Please enter your choice: '
+apt-get install -y openssl curl apache2 git mysql-client mysql-server php5 php5-cli php5-curl php5-mysql php5-mcrypt wget
+cd /tmp
+PS3='Which package would you like to install?'
 options=("Master (WWW)" "Slave (Scales)" "Master + Scales (WWW + Scales)" "Cancel")
 select opt in "${options[@]}"
 do
     case $opt in
         "Master (WWW)")
-            echo "you chose choice 1"
+            echo "Runnning installer for Master..."
+            wget https://raw.githubusercontent.com/delta360/PufferPanel-Better-Installer/master/ifiles/master.sh -O master.sh
+            chmod 755 /tmp/master.sh
+            /tmp/master.sh 
             ;;
         "Slave (Scales)")
-			wget
+            echo "Runnning installer for Slave..."
+            apt-get install -y lib32gcc1 htop screen openjdk-7-jre
+			wget https://raw.githubusercontent.com/delta360/PufferPanel-Better-Installer/master/ifiles/slave.sh -O slave.sh
+            chmod 755 /tmp/slave.sh
+            /tmp/slave.sh
             ;;
         "Master + Scales (WWW + Scales)")
-            echo "you chose choice 3"
+            wget https://raw.githubusercontent.com/delta360/PufferPanel-Better-Installer/master/ifiles/master.sh -O master.sh
+            wget https://raw.githubusercontent.com/delta360/PufferPanel-Better-Installer/master/ifiles/slave.sh -O slave.sh
+            chmod 755 /tmp/master.sh
+            chmod 755 /tmp/slave.sh
+            echo "Runnning installer for Master..."
+            /tmp/master.sh
+            echo ""
+            echo "Runnning installer for Slave..."
+            /tmp/slave.sh
             ;;
         "Cancel")
             break
@@ -25,3 +47,4 @@ do
         *) echo Invalid option selected;;
     esac
 done
+echo "Installation complete! If you require more help, please visit http://www.pufferpanel.com/v0.8.0/docs for more information."
